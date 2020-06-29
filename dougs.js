ad98a059b27c340f65650a4cf030d1a00396bae0
Doug Livingston <dliving1.dl@gmail.com>
2020-06-29 15:20:43 -0700

    Added artist name

--- dougs.js
+++ dougs.js
@@ -1,7 +1,7 @@
 var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
 var queryString = $.param({
     apikey: "4fcb0a30-b68a-11ea-b493-4bc132726a26",
-    title: "bird",
+    title: "deer",
     classification: "Paintings"
 });
 
@@ -12,7 +12,11 @@ $.getJSON(apiEndpointBaseURL + "?" + queryString, function(genpics) {
     let pictureImg = document.querySelector("#picture");
     pictureImg.setAttribute("src", genpics.records[0].images[0].baseimageurl);
 
+    let artistName = document.querySelector("#artist");
+    artistName.setAttribute("src", genpics.records[0].people[0].alphasort);
 
+    let artDate = document.querySelector("#date");
+    artDate.setAttribute("src", genpics.records[0].period);
 
     for (let index = 0; index < records.length; index++) {
         const element = records[index];
@@ -21,8 +25,8 @@ $.getJSON(apiEndpointBaseURL + "?" + queryString, function(genpics) {
 
 
         console.log(genpics.records[index].images[0].baseimageurl);
-
-
+        console.log(genpics.records[index].people[0].alphasort);
+        console.log(genpics.records[index].period);
 
     }
 
